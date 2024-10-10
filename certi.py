f8033e9d13d9b0f656ab8925d0f15dd8ef3e9179
Saswat Nayak <sknayak@wisc.edu>
2024-10-10 13:06:47 -0500

    RAG

--- certi.py
+++ certi.py
@@ -1,9 +1,3 @@
-import urllib.request
-import ssl
-
-context = ssl._create_unverified_context()
-urllib.request.urlopen("https://docs.aws.amazon.com/", context=context)
-
-import requests
-
-response = requests.get("https://docs.aws.amazon.com/", verify=False)
+version https://git-lfs.github.com/spec/v1
+oid sha256:070feb464bb4de37662ee408110072816b15a38e26e0c7fd75c99e5768cd4078
+size 236
